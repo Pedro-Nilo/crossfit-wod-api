79fb74bb933205539e23a24fb0e1f0a7329229dc
Pedro-Nilo <pedroniloz.nilo@gmail.com>
2023-08-09 20:22:22 -0300

    API com operações CRUD funcionais

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,21 +1,40 @@
+const { v4: uuid } = require("uuid");
+
+const Workout = require("../database/Workout");
+
 const getAllWorkouts = () => {
-    return;
+    const allWorkouts = Workout.getAllWorkouts();
+
+    return allWorkouts;
 };
 
-const getOneWorkout = () => {
-    return;
+const getOneWorkout = (workoutId) => {
+    const workout = Workout.getOneWorkout(workoutId);
+
+    return workout;
 };
 
-const createNewWorkout = () => {
-    return;
+const createNewWorkout = (newWorkout) => {
+    const workoutToInsert = {
+        ...newWorkout,
+        id: uuid(),
+        createdAt: new Date().toLocaleDateString("pt-BR", { timeZone: "UTC" }),
+        updatedAt: new Date().toLocaleDateString("pt-BR", { timeZone: "UTC" })
+    };
+
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+
+    return createdWorkout;
 };
 
-const updateOneWorkout = () => {
-    return;
+const updateOneWorkout = (workoutId, changes) => {
+    const updatedWorkout = Workout.updateOneWorkout(workoutId, changes);
+
+    return updatedWorkout;
 };
 
-const deleteOneWorkout = () => {
-    return;
+const deleteOneWorkout = (workoutId) => {
+    Workout.deleteOneWorkout(workoutId);
 };
 
 module.exports = {
